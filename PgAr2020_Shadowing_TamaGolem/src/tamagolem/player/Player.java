15241733fad95ebf7a822282a0d3250b7d7b3660
SimoneGiacomini <simogiaco77@gmail.com>
2020-05-27 18:07:27 +0200

    fatto una parte di Player

--- PgAr2020_Shadowing_TamaGolem/src/tamagolem/player/Player.java
+++ PgAr2020_Shadowing_TamaGolem/src/tamagolem/player/Player.java
@@ -1,6 +1,43 @@
 package tamagolem.player;
 
 public class Player {
-	
-	
+
+	private int currentGolem;
+
+	private String name;
+
+	public final static int G = 5;// PARAMETRO DA DECIDERE ASSIEME
+
+
+	public Player( String name) {
+		super();
+		this.currentGolem = G;
+		this.name = name;
+	}
+
+	/**
+	 * @return the currentGolem
+	 */
+	public int getCurrentGolem() {
+		return currentGolem;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public boolean hasLost() {
+		return getCurrentGolem() == 0;
+	}
 }
